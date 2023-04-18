75f9be3e9d497dd298305491c70bb7c56d673999
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-18 16:06:28 -0700

    added basic content for navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,32 @@
+import LittleLemonLogo from "../../icons_assets/Logo.svg";
 import "./Nav.css";
 
 function Nav() {
   return (
     <>
-      <nav></nav>
+      <nav>
+        <img src={LittleLemonLogo}></img>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Menu</a>
+          </li>
+          <li>
+            <a>Reservations</a>
+          </li>
+          <li>
+            <a>Order Online</a>
+          </li>
+          <li>
+            <a>Login</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
