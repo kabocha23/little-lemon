6afb766e0ce20c9d236169d7b237a2c3dfac3c24
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-18 16:07:25 -0700

    moved content to Nav component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,9 @@
-import LittleLemonLogo from "../../icons_assets/Logo.svg";
 import "./Header.css";
 
 function Header() {
   return (
     <>
-      <header>
-        <img src={LittleLemonLogo}></img>
-        <ul>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-          <li>
-            <a>Menu</a>
-          </li>
-          <li>
-            <a>Reservations</a>
-          </li>
-          <li>
-            <a>Order Online</a>
-          </li>
-          <li>
-            <a>Login</a>
-          </li>
-        </ul>
-      </header>
+      <header></header>
     </>
   );
 }
