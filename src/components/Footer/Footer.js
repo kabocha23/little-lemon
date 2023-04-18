f0e66e43a7f99dcbff2ea5f1435cfb772f4d0aa0
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-18 16:05:40 -0700

    added basic content for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,30 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-      <footer></footer>
+      <footer>
+        <img></img>
+        <ul>
+          <lh>Doormat Navigation</lh>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+        <ul>
+          <lh>Contact</lh>
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>Email</li>
+        </ul>
+        <ul>
+          <lh>Social Media Links</lh>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </footer>
     </>
   );
 }
