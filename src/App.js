4b4f41ba3ed748b4522f945115ecd0445cd29d17
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-19 15:13:44 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
+import About from "./components/About/About";
+import Specials from "./components/Specials/Specials";
+import ReserveTable from "./components/ReserveTable/ReserveTable";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 
@@ -9,7 +13,12 @@ function App() {
     <>
       <Header />
       <Nav />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/specials" element={<Specials />}></Route>
+        <Route path="/reserve-table" element={<ReserveTable />}></Route>
+      </Routes>
       <Footer />
     </>
   );
