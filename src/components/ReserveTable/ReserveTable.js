353f47bf2235c915adeee9bfae9b92e0f2642507
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-20 17:01:39 -0700

    imported the form component

---
+++ src/components/ReserveTable/ReserveTable.js
@@ -0,0 +1,13 @@
+import ReserveTableForm from "./components/ReserveTableForm/ReserveTableForm";
+import "./ReserveTable.css";
+
+function ReserveTable() {
+  return (
+    <>
+      <h1>Reserve Your Table!</h1>
+      <ReserveTableForm />
+    </>
+  );
+}
+
+export default ReserveTable;
