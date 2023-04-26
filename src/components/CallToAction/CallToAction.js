4012670bb5e53852f1fb97da4bbe177922ff90cc
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-26 14:43:44 -0700

    added basic content and structure

---
+++ src/components/CallToAction/CallToAction.js
@@ -0,0 +1,24 @@
+import "./CallToAction.css";
+
+function CallToAction() {
+  return (
+    <div className="calltoaction-container">
+      <div className="calltoaction-sub">
+        <div className="calltoaction-left">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button>Reserve a Table</button>
+        </div>
+        <div className="calltoaction-right">
+          <img></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CallToAction;
