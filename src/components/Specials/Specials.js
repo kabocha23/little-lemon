9bf10d687f6feb394820c125dba67e7dee174c70
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-27 14:36:23 -0700

    added basic content and structure

--- src/components/Specials/Specials.js
+++ src/components/Specials/Specials.js
@@ -1,7 +1,15 @@
 import "./Specials.css";
 
 function Specials() {
-  return <></>;
+  return (
+    <div className="specials-container">
+      <div className="specials-head">
+        <h1>This week's specials!</h1>
+        <button>Online Menu</button>
+      </div>
+      <div className="specials-main"></div>
+    </div>
+  );
 }
 
 export default Specials;
