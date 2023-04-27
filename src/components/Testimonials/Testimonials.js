5186ec0507f5be64ea3172344d1baab9cd8205c8
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-27 14:36:40 -0700

    added basic content and structure

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,7 +1,12 @@
 import "./Testimonials.css";
 
 function Testimonials() {
-  return <></>;
+  return (
+    <div className="testimonials-container">
+      <h1>Testimonials</h1>
+      <div className="testimonials-main"></div>
+    </div>
+  );
 }
 
 export default Testimonials;
